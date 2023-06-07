7ac24a9e7c849406b97a1bdba978aae69ffdd51d
fushi332218 <2087495494@qq.com>
2023-06-08 00:52:19 +0800

    modify KOB

---
+++ web/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <NavBar />
+  <router-view></router-view>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar'
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap"
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+body {
+  background-image: url("@/assets/images/background.png");
+  background-size: cover;
+}
+</style>
+
+
+
